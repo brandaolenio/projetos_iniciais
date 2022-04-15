c3044c267622f4d481feb9d0d68e9fc6caf622f2
LenioBrandao <lenioguerreiro@gmail.com>
2022-04-15 16:22:28 -0300

    Pequenas alterações

--- Calculadora_Subtracao_de_2_Numeros.py
+++ Calculadora_Subtracao_de_2_Numeros.py
@@ -1,5 +1,5 @@
 def sub():
-  print('Calculadora Subtração e Números Inteiros')
+  print('Calculadora Subtração de Números Inteiros')
   first_number = int(input('Digite o minuendo: '))
   second_number = int(input('Digite o subtraendo: '))
   subtracao = first_number - second_number
